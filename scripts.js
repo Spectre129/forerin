92a07601b97bf82c2187756b60348244232b783a
Spectre129 <basragavin@gmail.com>
2024-07-29 23:13:39 +0100

    final!!

--- scripts.js
+++ scripts.js
@@ -7,7 +7,47 @@ document.addEventListener("DOMContentLoaded",function()
 })
 })
 
+document.addEventListener("DOMContentLoaded",function()
+{
+    const button = document.getElementById("homeBtn");
+    button.addEventListener("click", function()
+{
+    window.location.href = "index.html";
+})
+})
+
 function openEdi()
+{
+    window.open("edi.html")
+}
+
+function openWaterfall()
+{
+    var width = window.innerWidth;
+    var height = window.innerHeight * 2;
+
+    // Calculate the position to center the popup
+    var left = (window.innerWidth - width) / 2;
+    var top = (window.innerHeight - height) / 2;
+
+    // Open the popup with the calculated dimensions and position
+    window.open('waterfall.html');
+}
+
+function openArbroath()
+{
+    var width = window.innerWidth;
+    var height = window.innerHeight * 2;
+
+    // Calculate the position to center the popup
+    var left = (window.innerWidth - width) / 2;
+    var top = (window.innerHeight - height) / 2;
+
+    // Open the popup with the calculated dimensions and position
+    window.open('arbroath.html');
+}
+
+function openStonehaven()
 {
     var width = window.innerWidth * 0.9;
     var height = window.innerHeight * 0.9   ;
@@ -17,5 +57,28 @@ function openEdi()
     var top = (window.innerHeight - height) / 2;
 
     // Open the popup with the calculated dimensions and position
-    window.open('edi.html', 'Popup', 'width=' + width + ',height=' + height + ',left=' + left + ',top=' + top);
+    window.open('stonehaven.html');
+}
+
+function openHazelhead()
+{
+    var width = window.innerWidth * 0.9;
+    var height = window.innerHeight * 0.9   ;
+
+    // Calculate the position to center the popup
+    var left = (window.innerWidth - width) / 2;
+    var top = (window.innerHeight - height) / 2;
+
+    // Open the popup with the calculated dimensions and position
+    window.open('hazelhead.html');
+}
+
+function openFaves()
+{
+    window.open("faves.html");
+}
+
+function photoGallery()
+{
+    window.location.href = "photos.html";
 }
